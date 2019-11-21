e267f25cb226c6cd6badb3acb09a2c939e1b586f
Brant Wagner <brawagne@uat.edu>
2019-11-20 20:09:28 -0700

    Second push
    added enemy and count down timer

---
+++ Cricket Learning/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+    public GameObject tar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = tar.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            RotateTowards(target.position);
+        }
+    }
+
+    private void RotateTowards(Vector2 target)
+    {
+        var offset = -90f;
+        Vector2 direction = target - (Vector2)transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * (angle + offset));
+    }   
+}
---
+++ Cricket Learning/Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("test");
+        SceneManager.LoadScene("Bug");
+
+    }
+}
